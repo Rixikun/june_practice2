538bfa45b9accf54b3bcf21e02b9264927ef0e55
LinXia <linyianxia@hotmail.com>
2020-06-09 21:15:22 -0400

    set up reducers

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CakeContainer = () => {
+    return (
+        <div>
+            <h2>Number of Cakes</h2>
+            <button>
+                Buy cakes
+            </button>
+        </div>
+    )
+}
+
+export default CakeContainer;
