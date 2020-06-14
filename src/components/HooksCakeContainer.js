d807778a273d8ca6c91c0176fa404d0d90e0fb84
LinXia <linyianxia@hotmail.com>
2020-06-14 11:25:35 -0400

    add: useDispatch as alt to mapDispatchToProps, warning not hooks exactly the same as connect()

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { buyCake } from '../redux'
 
 const HooksCakeContainer = () => {
     //access redux store's state
     //takes in function & returns value
     const numOfCakes = useSelector(state => state.numOfCakes)
-
+    //returns a reference to the dispatch fn from the redux store
+    //use to dispatch actions
+    const dispatch = useDispatch()
     return (
         <div>
             <h2>Num of Cakes - {numOfCakes}</h2>
-            <button>Buy Cake</button>
+            <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
         </div>
     )
 }
